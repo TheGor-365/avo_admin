949a64b0717a5acbffdf2b170b14ebd8ad1aa729
Gor <61106836+TheGor-365@users.noreply.github.com>
2023-03-04 01:08:42 +0300

    Add posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+  resources :posts
+  mount Avo::Engine, at: Avo.configuration.root_path
 end
